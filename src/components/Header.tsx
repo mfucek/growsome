dc9eee50c01a1b4c8e61a201073f4169576ddf60
Matija Fućek <matijafucek1@gmail.com>
2023-03-21 15:12:58 +0100

    fancy header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,38 @@
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
 import { Container } from './Container';
 
 export const Header = () => {
+	const [scrolled, setScrolled] = useState(false);
+	useEffect(() => {
+		let a = setInterval(() => {
+			// console.log(document.documentElement.scrollTop);
+			// console.log(document.documentElement.clientHeight);
+			setScrolled(
+				document.documentElement.scrollTop >=
+					document.documentElement.clientHeight
+			);
+		}, 100);
+		return () => {
+			clearInterval(a);
+		};
+	}, []);
 	return (
 		<>
 			<div className="fixed w-screen z-40">
 				<Container>
-					<div className="p-12">
+					<div
+						className={classNames(
+							'p-12 duration-300',
+							scrolled && 'bg-white/80 backdrop-blur-md'
+						)}
+					>
 						<img
 							src="/logo.png"
 							alt="Growsome"
 							className="h-6 hover:translate-y-[-2px] active:translate-y-[4px] duration-300 active:duration-100 cursor-pointer hover:opacity-80 active:opacity-60"
 							onClick={() => {
-								document.documentElement.scrollTo({ top: 0 });
+								window.scrollTo({ top: 0, behavior: 'smooth' });
 							}}
 						/>
 					</div>
